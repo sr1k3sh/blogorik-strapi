4a0a97ece5aff07ec4e88fed314320fdb2aa5d8c
sr1k3sh <sr1k3sh@gmail.com>
2023-07-09 18:12:41 +0900

    upload code

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,21 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          // "img-src": ["'self'", "https: data:"],
+          'img-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com', 'storage.cloud.google.com', 'market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com', 'storage.googleapis.com', 'market-assets.strapi.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  // 'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
