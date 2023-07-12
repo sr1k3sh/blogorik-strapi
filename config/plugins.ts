0b51bc5970836fbc8f0ef9dc8618d66dad596123
sr1k3sh <sr1k3sh@gmail.com>
2023-07-12 18:38:34 +0900

    updated limit of graphql

--- config/plugins.ts
+++ config/plugins.ts
@@ -30,7 +30,7 @@ module.exports = ({env}) => ({
       endpoint: '/graphql',
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 7,
+      depthLimit: 10,
       amountLimit: 100,
       apolloServer: {
         tracing: false,
