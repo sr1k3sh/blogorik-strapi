211fd8160b2e0fa76e46fde6a86b6743a7291ac8
sr1k3sh <sr1k3sh@gmail.com>
2023-08-10 17:28:24 +0900

    updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -34,5 +34,13 @@ module.exports = ({ env }) => ({
   "video-field":{
     enabled:true
   },
+  'users-permissions': {
+    provider: 'google',
+    providerOptions: {
+      clientId: process.env.G_CLIENT_ID,
+      clientSecret: process.env.G_CLIENT_SECRET,
+      redirectUri: 'http://localhost:1337/connect/google/callback', // e.g., http://localhost:1337/connect/google/callback
+    },
+  },
   //...
 })
