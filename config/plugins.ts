077a8210d300ee3554e771fbf61c39a1fed64251
sr1k3sh <sr1k3sh@gmail.com>
2023-08-10 17:30:44 +0900

    udpated

--- config/plugins.ts
+++ config/plugins.ts
@@ -39,7 +39,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       clientId: process.env.G_CLIENT_ID,
       clientSecret: process.env.G_CLIENT_SECRET,
-      redirectUri: 'http://localhost:1337/connect/google/callback', // e.g., http://localhost:1337/connect/google/callback
+      redirectUri: '/connect/google/callback', // e.g., http://localhost:1337/connect/google/callback
     },
   },
   //...
